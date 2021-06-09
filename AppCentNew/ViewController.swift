ce12830119a2e4e0a0a74a560e79610925816833
ArifOk <ArifOk@garantibbva.com.tr>
2021-06-09 13:46:52 +0300

    Initial Commit

---
+++ AppCentNew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppCentNew
+//
+//  Created by ArifOk on 9.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
